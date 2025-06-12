d33db2bc062bde929a63152c22ac68214377fe7b
Cornee van Blerk <corneevanblerk@gmail.com>
2025-06-12 17:28:19 +0200

    feat: add project nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,22 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local telescope = require("telescope")
-		telescope.setup({})
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local telescope = require("telescope")
+        telescope.setup({})
 
-		local builtin = require("telescope.builtin")
-		local utils = require("telescope.utils")
+        local builtin = require("telescope.builtin")
+        local utils = require("telescope.utils")
 
-		vim.keymap.set("n", "<leader>pf", function()
-			builtin.find_files({ cwd = utils.buffer_dir() })
-		end, { desc = "Find Files in Buffer Dir" })
+        vim.keymap.set("n", "<leader>pf", function()
+            require("telescope.builtin").find_files({
+                cwd = require("project_nvim.project").get_project_root()
+            })
+        end, { desc = "Find Files in Project Root" })
 
-		vim.keymap.set("n", "<leader>ps", function()
-			builtin.live_grep({ cwd = utils.buffer_dir() })
-		end, { noremap = true, silent = true, desc = "Live Grep in Buffer Dir" })
-	end,
+        vim.keymap.set("n", "<leader>ps", function()
+            require('telescope.builtin').live_grep()
+        end, { noremap = true, silent = true, desc = "Live Grep in Project Root" })
+    end,
 }
