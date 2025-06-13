725cc670c248367e899668b2343d518c56c20dab
Rationelis <corneevanblerk@gmail.com>
2025-06-13 11:33:03 +0200

    feat: add ts lsp

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -1,6 +1,26 @@
 return {
-	"mrcjkb/rustaceanvim",
-	version = "^6",
-	lazy = false,
-	ft = "rust",
+    {
+        "mrcjkb/rustaceanvim",
+        version = "^6",
+        lazy = false,
+        ft = "rust",
+    },
+
+ {
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {
+			settings = {
+				tsserver_file_preferences = {
+					includeInlayParameterNameHints = "all",
+					includeCompletionsForModuleExports = true,
+					quotePreference = "auto",
+				},
+				tsserver_format_options = {
+					allowIncompleteCompletions = false,
+					allowRenameOfImportPath = false,
+				},
+			},
+		},
+	},
 }
